5ee744f610c05eedd5b2384dad2b5d8f2b8544b5
OG1Kanobe <tiroally@gmail.com>
2026-02-11 11:17:14 +0200

    tryijg to figuee out why app aint rendering

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@ export default {
     providers: [
       {
         // This tells Convex "If a request comes from this URL, it's my app"
-        domain: process.env.CONVEX_SITE_URL,
+        domain: process.env.SITE_URL || process.env.CONVEX_SITE_URL,
         applicationID: "convex",
       },
     ],
